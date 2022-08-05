6b82d343afa74f75000e128b762353fd0290986b
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-08-05 15:29:02 -0700

    Added styles

--- src/Display.js
+++ src/Display.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+
 let Display = ({info}) => {
     return (
         <div>
             {
-                <div>
+                <div className="card-container justify-content-around d-flex flex-wrap ">
                 {
                     (info.length > 0) && info.map((details, index) => (
-                        <div key={index}>
-                            <img src={details.picture.large} />
-                            <div >
+                        <div className="d-flex gap-3 card" key={index}>
+                            <img src={details.picture.large} className="m-2 border rounded-circle" />
+                            <div className="d-flex flex-column p-5" >
                                 <span>{details.name.title}{" "}{details.name.first}{" "}{details.name.last}</span>
                                 <span>{details.email}</span>
                                 <span>{details.location.country}</span>
